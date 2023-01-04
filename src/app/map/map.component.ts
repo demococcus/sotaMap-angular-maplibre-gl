b2ddce76ce5ff1707aaff47f608c77a9a5a954ed
Georgi <georgi.gatin@gmail.com>
2023-01-04 18:55:14 -0500

    initial commit

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,153 @@
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Map, NavigationControl, Marker, Popup   } from 'maplibre-gl';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  map: Map | undefined;
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+ 
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit() {
+    const initialState = { lng: 23.50206819, lat: 42.15451878, zoom: 10 };
+
+    const rilaSotaPeaks = {"type":"FeatureCollection","features":[
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.27400962,42.17570386]},"id":"26bbc383-068e-41cd-a008-a28808e19ffa","properties":{"name":"Kalinin vrah","sota":"LZ/RL-013","type":"peak","icon":"mountain","gowhere":"https://gowhere.bg/en/places/mountains/kalinite-peaks/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.31144252,42.07682725]},"id":"2492fa6f-dba2-4dac-b8ed-ee860ce2609d","properties":{"name":"Trsarev vrah","sota":"LZ/RL-026","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.86364284,42.12693415]},"id":"166e9b3b-076d-4f89-aaea-5c2f90806657","properties":{"name":"Balabanitsa","sota":"LZ/RL-030","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.53009191,42.12723334]},"id":"3390494e-9da0-4097-ba23-69e2728703b4","properties":{"name":"Shishkovitsa","sota":"LZ/RL-012","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.42435923,42.02097097]},"id":"b02310e8-8429-4f5e-abf4-579ecd8bda5e","properties":{"name":"Ezernik","sota":"LZ/RL-023","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.57222405,42.10205679]},"id":"9d829905-20ef-40cf-bf94-ab9cadeb331f","properties":{"name":"Nalbant (Kovach)","sota":"LZ/RL-018","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.39050117,42.05998551]},"id":"fb104dae-25c7-475c-a78a-716059429fd0","properties":{"name":"Gorna kadiytsa","sota":"LZ/RL-028","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.58528748,42.17928968]},"id":"be477545-22fd-4bee-9e8e-5bc7a98a02a0","properties":{"name":"Musala","sota":"LZ/RL-001","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.40000477,42.00895358]},"id":"10d4d8c9-4c0c-42e9-8e38-cbef8502f5e4","properties":{"name":"Parangalitsa","sota":"LZ/RL-025","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.53425685,42.19258164]},"id":"7b7b4193-84cd-4e8c-9f02-2c86dfa03faa","properties":{"name":"Buchkata","sota":"LZ/RL-024","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.36306539,42.17385125]},"id":"48859fe7-d900-48a8-a06d-2bf34d42af78","properties":{"name":"Malyovitsa","sota":"LZ/RL-003","type":"peak","icon":"mountain","gowhere":"https://gowhere.bg/en/places/mountains/malyovitsa-peak/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.59273759,42.11153519]},"id":"5801e29c-2345-460d-b6f4-264fd422bfb3","properties":{"name":"Suha vapa","sota":"LZ/RL-016","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.46144024,42.11688983]},"id":"3d03ce77-23c3-4ee5-abe7-14514578941c","properties":{"name":"Rilets","sota":"LZ/RL-005","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.29417339,42.19876559]},"id":"1678b0d7-85ba-4826-9a13-fab07e524a61","properties":{"name":"Otovishki vrah","sota":"LZ/RL-009","type":"peak","icon":"mountain","gowhere":"https://gowhere.bg/en/places/mountains/otovishki-vrah/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.45688906,42.06540186]},"id":"5647f1c9-fe96-4286-921b-03581b645c4b","properties":{"name":"Angelov vrah","sota":"LZ/RL-015","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.36192814,42.07103437]},"id":"9b542df6-0cbe-4dca-9c6c-d3893049c07d","properties":{"name":"Dolna Kadiytsa","sota":"LZ/RL-027","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.81609049,42.13800795]},"id":"389dd52a-bc1a-4485-acd2-b7ec255774ca","properties":{"name":"Slavov vrah","sota":"LZ/RL-029","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.39739552,42.17466704]},"id":"939e02a6-ec02-4462-98f6-9d06214f8880","properties":{"name":"Golyam Kupen","sota":"LZ/RL-002","type":"peak","icon":"mountain","gowhere":"https://gowhere.bg/en/places/mountains/peak-golyam-kupen/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.53437057,42.15987775]},"id":"6f71c8b7-b4ac-49ea-a609-b8e8389c28fb","properties":{"name":"Trite mushi","sota":"LZ/RL-006","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.71179548,42.17286687]},"id":"18a25f0b-f73d-4552-837a-53611659e88d","properties":{"name":"Ibar","sota":"LZ/RL-014","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.50678459,42.11678319]},"id":"fe861492-bfd6-43f1-89ff-42316276f309","properties":{"name":"Yosifitsa","sota":"LZ/RL-008","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.48187652,42.09077926]},"id":"f95f2713-0e9d-476a-927b-21d1812f4f53","properties":{"name":"Karaakanitsa","sota":"LZ/RL-004","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.68980566,42.18564346]},"id":"df46ad6b-cd2b-4751-b801-763493e3c37c","properties":{"name":"Kota 2500","sota":"LZ/RL-022","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.43387786,42.18540655]},"id":"475037f7-939a-4b68-8317-f782699ebb3b","properties":{"name":"Lopushki vrah","sota":"LZ/RL-007","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.46364608,42.15164903]},"id":"e6feab04-2574-46ce-b0fb-47829f981fa6","properties":{"name":"Chal","sota":"LZ/RL-011","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.74424163,42.18364008]},"id":"3e0a5c7c-d4b9-4ef1-b30c-a664f2e8b80a","properties":{"name":"Ravni chal","sota":"LZ/RL-017","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.65423742,42.17510753]},"id":"b4fe6d27-e9cd-4edb-b1d1-9e9d56e94769","properties":{"name":"Mustachal (Zavratchitsa)","sota":"LZ/RL-019","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.7715551,42.18039162]},"id":"6e8975fe-9e42-4d00-8261-3b8afb292ca8","properties":{"name":"Belmeken","sota":"LZ/RL-020","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.50206819,42.15451878]},"id":"cf09a1d1-4ae4-400a-bbea-af21d4e4600b","properties":{"name":"Yozola","sota":"LZ/RL-010","type":"peak","icon":"mountain","gowhere":null}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.30364695,42.17339961]},"id":"571a544f-8bf0-4599-96e2-62f9a0c3239f","properties":{"name":"Varla","sota":"LZ/RL-021","type":"peak","icon":"mountain","gowhere":null}},
+  
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.38381711,42.17422178]},"id":"65a520fb-5cb4-41f6-9ab6-08d45b82617d","properties":{"name":"Orlovets","type":"shelter","icon":"shelter","gowhere":"https://gowhere.bg/en/places/mountains/orlovets-shelter/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.64082637,42.19023925]},"id":"3e8653ea-b918-496f-9e39-65c57a0b4d2c","properties":{"name":"Maritsa","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.32325283,42.24362735]},"id":"2ebf730b-2d0f-4aec-895e-b55abfc26045","properties":{"name":"Pionerska","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.40593782,42.15473612]},"id":"bff189ca-0199-410f-996e-61da31d1e36b","properties":{"name":"Baza Yavora","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.46281701,42.00677023]},"id":"e973e678-0c76-4c15-8afa-1d2ef652f950","properties":{"name":"Dobyrsko","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.33341947,42.04430038]},"id":"d6604d45-7f38-45d5-9b7a-800138b918b9","properties":{"name":"Bodrost","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.74913397,42.19286084]},"id":"0443f27d-41ca-497d-81c9-3e791a9dd9bb","properties":{"name":"Belmeken","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.37426844,42.1887591]},"id":"02f441e4-d25b-4619-a228-d572abb8420c","properties":{"name":"Malyovitsa","icon":"lodging","type":"hut","gowhere":"https://gowhere.bg/en/places/mountains/malyovitsa-hut/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.39951554,42.18093542]},"id":"0387e50c-52c4-4af8-9a7d-5825bf494706","properties":{"name":"Scary Lake","icon":"shelter","type":"shelter","gowhere":"https://gowhere.bg/en/places/mountains/strashnoto-ezero-shelter/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.28359476,42.18976866]},"id":"18c5db29-ad59-42ce-a5ce-6c41fc0ecf33","properties":{"name":"Ivan Vazov","icon":"lodging","type":"hut","gowhere":"https://gowhere.bg/en/places/mountains/ivan-vazov-hut/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.53443494,42.05264024]},"id":"0ccaac7d-a5ee-42ab-a3b3-bf91d920d125","properties":{"name":"Semkovo","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.58747402,42.19824359]},"id":"1e57625e-e2ad-4733-9417-e8b008f73092","properties":{"name":"Musala","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.43962207,42.04716863]},"id":"1fe66561-66b3-480e-a5e6-780caed34f26","properties":{"name":"Macedonia","icon":"lodging","type":"hut","gowhere":"https://gowhere.bg/en/places/mountains/macedonia-hut/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.3241047,42.20761381]},"id":"6e492137-0687-4cd6-baae-f477a925eb7a","properties":{"name":"7 lakes","icon":"lodging","type":"hut","gowhere":"https://gowhere.bg/en/places/mountains/sedemte-ezera-mountain-hut/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.35856974,42.20637327]},"id":"9e41c3c7-f13d-490a-a897-5092bc194e75","properties":{"name":"Kire Chuguno","icon":"shelter","type":"shelter","gowhere":"https://gowhere.bg/en/places/mountains/kire-chuguno%E2%80%99-shelter/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.58123627,42.22642636]},"id":"b3a7b165-fa1b-43ad-9a3d-1dabbbcd5f04","properties":{"name":"Yastrebetz","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.64080063,42.16830997]},"id":"8ff66223-9c04-4ec8-95d5-815d1813506f","properties":{"name":"Zavrachica","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.59347144,42.12176756]},"id":"7ab08216-ad20-4ab5-8838-b2c846899664","properties":{"name":"Grunchar","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.3216714,42.21988599]},"id":"c17b672a-3cc7-4824-a966-a916aac04da6","properties":{"name":"Rila lakes","icon":"lodging","type":"hut"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.45202112,42.17254822]},"id":"a510fc10-bed3-49de-87ee-e6a38f13f97d","properties":{"name":"Kobilino","icon":"shelter","type":"shelter","gowhere":"https://gowhere.bg/en/places/mountains/kobilino-branishte-shelter/"}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[23.49378982,42.11235437]},"id":"f84d551f-d173-4d8f-8162-2511badd1d40","properties":{"name":"Fish Lakes","icon":"lodging","type":"hut","gowhere":"https://gowhere.bg/en/places/mountains/ribni-ezera/"}}
+      
+  
+      ]}
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
+    });
+
+    this.map.addControl(new NavigationControl({}), 'top-right');
+
+    // new Marker({color: "#FF0000"})
+    //   .setLngLat([23.5020,42.1545])
+    //   .addTo(this.map);
+
+    this.map.on('load', function(ev) {
+
+      let map = ev.target
+
+      map.addSource('places-source', {
+        type: 'geojson',
+        'data': rilaSotaPeaks
+      });
+
+      map.addLayer({
+        'id':'places',
+        'type':'symbol',
+        'source':'places-source',
+        'layout':{
+          'icon-image':'{icon}_15',
+          'icon-overlap':'always',
+          'visibility':'visible',
+        }
+      });
+
+      
+    })
+
+  
+    this.map.on('click', 'places', (e) => {
+
+      const coordinates = e.features[0].geometry['coordinates'];
+      const name = e.features[0].properties.name;
+      const sotaID = e.features[0].properties.sota;
+      const gowhere = e.features[0].properties.gowhere;
+
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 :-360;
+      }
+
+      let content = `<p class="popupTitle"> ${name} </p>`
+
+      if (sotaID != undefined) {
+        content += `<p class="popupText"><a href="https://sotl.as/summits/${sotaID}" target="_blank">${sotaID}</a></p>`
+      }
+      if (gowhere != undefined) {
+          content += `<p class="popupText"><a href="${gowhere}" target="_blank">GOWHERE</a></p>`
+      }
+
+      new Popup()
+        .setLngLat(coordinates)
+        .setHTML(content)
+        .addTo(this.map);
+
+    });
+
+
+}
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+}
